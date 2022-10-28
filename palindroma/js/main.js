ebec73c0a4bb2d1321404e99282af274821c0374
Alelisk91 <alessandrofabbretti91@gmail.com>
2022-10-28 16:02:36 +0200

    Aggiunto prompt per richiesta all'utente

---
+++ palindroma/js/main.js
@@ -0,0 +1,9 @@
+//Chiedere all’utente di inserire una parola
+//Creare una funzione per capire se la parola inserita è palindroma
+
+"use stricy"
+
+//chiedo all'utente di inserire una parola
+
+const word = prompt ("Inserisci una parola")
+
