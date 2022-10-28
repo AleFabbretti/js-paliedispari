9d9eb6775de117e4541221f1a31ef5c8afe8422e
Alelisk91 <alessandrofabbretti91@gmail.com>
2022-10-28 16:29:38 +0200

    Aggiunta funzione e ricarica pagina

--- palindroma/js/main.js
+++ palindroma/js/main.js
@@ -5,5 +5,29 @@
 
 //chiedo all'utente di inserire una parola
 
-const word = prompt ("Inserisci una parola")
+const user = prompt ("Inserisci una parola")
+
+let finalWord = "";
+
+//creo la mia funzione
+function palindroma (word) {
+
+    //assegno un ciclo for che decrementa dalla fine della parola all'inizio
+    for ( let i = user.length -1; i >= 0; i--) {
+        finalWord += user[i];
+    }
+
+    //creo una condizione
+    if ( user === finalWord) {
+        alert("la parola è palindroma")
+    }else{
+        alert("la parola non è palindroma")
+    }
+    
+
+}
+
+palindroma(user);
+
+window.location.reload()
 
