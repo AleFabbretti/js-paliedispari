00c8beae07971c6ac9eaedd341204d8e1f268a31
Alelisk91 <alessandrofabbretti91@gmail.com>
2022-10-28 16:38:13 +0200

    Aggiunti prompt di richiesta all'utente

---
+++ pariedispari/js/main.js
@@ -0,0 +1,24 @@
+/*L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+Sommiamo i due numeri
+Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+Dichiariamo chi ha vinto.
+*/
+
+"use strict"
+
+/* ------FUNCTION------*/
+
+//genero un numero random da 1 a 5 per la cpu
+
+
+
+/* ------MAIN------*/
+
+//chiediamo all'utente di scegliere tra pari o dispari
+const choise = prompt("Scegli tra pari o dispari");
+
+//chiediamo all'utente di scegliere un numero tra 1 e 5
+const numberChoise = prompt("Scegli un numero tra 1 e 5");
+
+
