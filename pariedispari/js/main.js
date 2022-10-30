a0dd1f469bdb24db0b2df5fe3df96b13040611b7
Alelisk91 <alessandrofabbretti91@gmail.com>
2022-10-30 19:03:21 +0100

    Aggiunta condizione e ricarica della pagina automatica

--- pariedispari/js/main.js
+++ pariedispari/js/main.js
@@ -9,8 +9,23 @@ Dichiariamo chi ha vinto.
 
 /* ------FUNCTION------*/
 
-//genero un numero random da 1 a 5 per la cpu
+//genero un numero random da 1 a 5 per la cpu attraverso una funzione
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
+
+//creo una funzione per stabilire se la somma dei due numeri è pari o dispari 
+
+function winner(number){
+    //creo una variabile
+
+    if(number % 2 === 0){
+        return true;
+    }
+    return false;
+
+}
 
 
 /* ------MAIN------*/
@@ -19,6 +34,22 @@ Dichiariamo chi ha vinto.
 const choise = prompt("Scegli tra pari o dispari");
 
 //chiediamo all'utente di scegliere un numero tra 1 e 5
-const numberChoise = prompt("Scegli un numero tra 1 e 5");
+const numberChoise = Number(prompt("scegli un numero da 1 a 5"));
 
+const cpuNumber = getRndInteger(1, 5);
+console.log(cpuNumber);
 
+const total = numberChoise + cpuNumber;
+console.log(total);
+
+console.log(winner(total));
+
+if (winner(total) === true && choise === "pari"){
+    alert("Hai vinto")
+}else if (winner(total) === false && choise === "dispari"){
+    alert("Hai vinto");
+} else {
+    alert("Hai perso");
+}
+
+window.location.reload();
